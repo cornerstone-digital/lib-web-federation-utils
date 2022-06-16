ba88ed9f312cff961723c58a9a76fabddd21adc7
Martin Egan <martingegan@gmail.com>
2022-06-16 09:24:39 +0100

    feat(core): expose helpers from main export

--- packages/federated-core/src/index.ts
+++ packages/federated-core/src/index.ts
@@ -4,6 +4,7 @@ import { FederatedRuntime } from './runtime'
 export * from './utils'
 export * from './runtime'
 export * from './types'
+export * from './helpers'
 
 if (environmentUtils.isBrowser() && !window.__FEDERATED_CORE__) {
   // eslint-disable-next-line no-console
