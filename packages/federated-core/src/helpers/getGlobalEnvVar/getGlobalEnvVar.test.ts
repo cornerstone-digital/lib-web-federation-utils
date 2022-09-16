cb68c0f8032960f9a4336b39d42931fda0771a3f
Martin Egan <martingegan@gmail.com>
2022-09-16 14:43:28 +0100

    feat: general cleanup and new helpers

--- packages/federated-core/src/helpers/getGlobalEnvVar/getGlobalEnvVar.test.ts
+++ packages/federated-core/src/helpers/getGlobalEnvVar/getGlobalEnvVar.test.ts
@@ -3,7 +3,9 @@ import getGlobalEnvVar from './getGlobalEnvVar'
 describe('getGlobalEnvVar', () => {
   it('should return the value of the global variable', () => {
     window.VFUK = {
-      test: 'test',
+      env: {
+        test: 'test',
+      },
     }
 
     expect(getGlobalEnvVar('test')).toEqual('test')
@@ -11,8 +13,10 @@ describe('getGlobalEnvVar', () => {
 
   it('should return the value of the global variable in the namespace', () => {
     window.VFUK = {
-      test: {
-        test: 'test',
+      env: {
+        test: {
+          test: 'test',
+        },
       },
     }
 
@@ -27,13 +31,25 @@ describe('getGlobalEnvVar', () => {
     )
   })
 
+  it('should throw an error if the VFUK.env is not defined', () => {
+    window.VFUK = {
+      env: undefined,
+    }
+
+    expect(() => getGlobalEnvVar('test')).toThrowError(
+      'window.VFUK.env is not defined'
+    )
+  })
+
   it('should throw an error if the global variable in the namespace is not defined', () => {
     window.VFUK = {
-      test: undefined,
+      env: {
+        test: undefined,
+      },
     }
 
     expect(() => getGlobalEnvVar('test', 'test')).toThrowError(
-      'window.VFUK.test is not defined'
+      'window.VFUK.env.test is not defined'
     )
   })
 })
--- packages/federated-core/src/helpers/getGlobalEnvVar/getGlobalEnvVar.ts
+++ packages/federated-core/src/helpers/getGlobalEnvVar/getGlobalEnvVar.ts
@@ -3,11 +3,16 @@ const getGlobalEnvVar = (varName: string, namespace?: string) => {
     throw new Error('window.VFUK is not defined')
   }
 
-  const vfukVar: string | Record<string, unknown> = window.VFUK
+  if (!window.VFUK['env']) {
+    throw new Error('window.VFUK.env is not defined')
+  }
+
+  const vfukVar: string | Record<string, unknown> =
+    (window.VFUK['env'] as Record<string, unknown>) || {}
 
   if (vfukVar && namespace) {
     if (!vfukVar[namespace]) {
-      throw new Error(`window.VFUK.${namespace} is not defined`)
+      throw new Error(`window.VFUK.env.${namespace} is not defined`)
     }
     const namespaceVar = vfukVar[namespace] as Record<string, unknown>
 
