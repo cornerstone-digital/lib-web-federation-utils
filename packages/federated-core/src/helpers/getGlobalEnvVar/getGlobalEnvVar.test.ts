5ed9521c611412b568de53345c9623b87ba77ec8
Martin Egan <martingegan@gmail.com>
2022-09-16 14:43:28 +0100

    feat: general cleanup and new helpers

--- packages/federated-core/src/helpers/getGlobalEnvVar/getGlobalEnvVar.test.ts
+++ packages/federated-core/src/helpers/getGlobalEnvVar/getGlobalEnvVar.test.ts
@@ -1,5 +1,9 @@
 import getGlobalEnvVar from './getGlobalEnvVar'
 
+const consoleErrorSpy = jest
+  .spyOn(console, 'error')
+  .mockImplementation(() => null)
+
 describe('getGlobalEnvVar', () => {
   it('should return the value of the global variable', () => {
     window.VFUK = {
@@ -26,30 +30,18 @@ describe('getGlobalEnvVar', () => {
   it('should throw an error if the global variable is not defined', () => {
     window.VFUK = undefined
 
-    expect(() => getGlobalEnvVar('test')).toThrowError(
-      'window.VFUK is not defined'
-    )
+    getGlobalEnvVar('test')
+
+    expect(consoleErrorSpy).toBeCalledWith('window.VFUK is not defined')
   })
 
-  it('should throw an error if the VFUK.env is not defined', () => {
+  it('should log error if the VFUK.env is not defined', () => {
     window.VFUK = {
       env: undefined,
     }
 
-    expect(() => getGlobalEnvVar('test')).toThrowError(
-      'window.VFUK.env is not defined'
-    )
-  })
+    getGlobalEnvVar('test')
 
-  it('should throw an error if the global variable in the namespace is not defined', () => {
-    window.VFUK = {
-      env: {
-        test: undefined,
-      },
-    }
-
-    expect(() => getGlobalEnvVar('test', 'test')).toThrowError(
-      'window.VFUK.env.test is not defined'
-    )
+    expect(consoleErrorSpy).toBeCalledWith('window.VFUK.env is not defined')
   })
 })
--- packages/federated-core/src/helpers/getGlobalEnvVar/getGlobalEnvVar.ts
+++ packages/federated-core/src/helpers/getGlobalEnvVar/getGlobalEnvVar.ts
@@ -1,25 +1,27 @@
 const getGlobalEnvVar = (varName: string, namespace?: string) => {
   if (!window.VFUK) {
-    throw new Error('window.VFUK is not defined')
+    console.error('window.VFUK is not defined')
+
+    return
   }
 
   if (!window.VFUK['env']) {
-    throw new Error('window.VFUK.env is not defined')
+    console.error('window.VFUK.env is not defined')
+
+    return
   }
 
-  const vfukVar: string | Record<string, unknown> =
-    (window.VFUK['env'] as Record<string, unknown>) || {}
+  const vfukVar: string | Record<string, unknown> = window.VFUK[
+    'env'
+  ] as Record<string, unknown>
 
-  if (vfukVar && namespace) {
-    if (!vfukVar[namespace]) {
-      throw new Error(`window.VFUK.env.${namespace} is not defined`)
-    }
+  if (namespace && vfukVar[namespace]) {
     const namespaceVar = vfukVar[namespace] as Record<string, unknown>
 
-    return namespaceVar[varName]
-  } else {
-    return vfukVar[varName]
+    return namespaceVar[varName] || vfukVar[varName]
   }
+
+  return vfukVar[varName]
 }
 
 export default getGlobalEnvVar
