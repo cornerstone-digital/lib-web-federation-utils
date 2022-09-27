2f1f81707b0e93eed820f844c4f8bf8c3bbcbf03
Martin Egan <martingegan@gmail.com>
2022-09-27 17:22:52 +0100

    feat: update to support passing loading component

--- packages/federated-react/src/components/FederatedModuleLoader/FederatedModuleLoader.tsx
+++ packages/federated-react/src/components/FederatedModuleLoader/FederatedModuleLoader.tsx
@@ -17,17 +17,24 @@ function FederatedModuleLoader<PropTypes>({
   mountId = `${scope}-${name}-${v4()}`,
   loadingComponent = <div>Loading...</div>,
 }: FederatedModuleLoaderProps<PropTypes>) {
+  let isLoading = true
   if (typeof window !== 'undefined') {
     const module = getFederatedModule({ scope, name, basePath })
 
     module.then((module: FederatedModule | undefined) => {
       if (module?.mount) {
+        isLoading = false
         void module.mount(props, mountId)
       }
     })
   }
 
-  return <div id={mountId}>{loadingComponent}</div>
+  return (
+    <>
+      {isLoading && loadingComponent}
+      <div id={mountId}></div>
+    </>
+  )
 }
 
 export default FederatedModuleLoader
